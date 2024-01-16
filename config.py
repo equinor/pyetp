253dd1136fbbb6b6043b04302b3d97e74a54f6fe
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2024-01-16 12:06:35 +0000

    Add initial testing structure

---
+++ config.py
@@ -0,0 +1,2 @@
+ETP_SERVER_URL = "ws://localhost:9002"
+DATASPACE = "test/pss-data-gateway"
---
+++ test_etp_client.py
@@ -0,0 +1,38 @@
+import unittest
+import asyncio
+
+import numpy as np
+import xtgeo
+
+import map_api.etp_client as etp_client
+
+from .config import ETP_SERVER_URL, DATASPACE
+
+
+class TestETPClient(unittest.TestCase):
+    def test_rddms_roundtrip(self):
+        ncol, nrow = 3, 4
+        surface = xtgeo.RegularSurface(
+            ncol=ncol,
+            nrow=nrow,
+            xori=np.random.rand() * 1000,
+            yori=np.random.rand() * 1000000,
+            xinc=23.0,
+            yinc=47.3,
+            rotation=33.0,
+            values=np.random.random((nrow, ncol)),
+        )
+
+        rddms_urls = asyncio.run(
+            etp_client.upload_xtgeo_surface_to_rddms(
+                surface, "test-surface", ETP_SERVER_URL, DATASPACE, ""
+            )
+        )
+
+        assert len(rddms_urls) == 3
+
+        asyncio.run(
+            etp_client.download_xtgeo_surface_from_rddms(
+                rddms_urls, ETP_SERVER_URL, DATASPACE, ""
+            )
+        )
