742fe981436c0014272704ff2941d0e365231c97
Marc Listemann <106686378+marclistemann@users.noreply.github.com>
2024-01-30 11:50:40 +0000

    Fix delete_resqml_objects function

--- etp_client/__init__.py
+++ etp_client/__init__.py
@@ -1,1 +1,1 @@
-from .etp_client import upload_xtgeo_surface_to_rddms, download_xtgeo_surface_from_rddms, download_resqml_surface
+from .etp_client import upload_xtgeo_surface_to_rddms, download_xtgeo_surface_from_rddms, download_resqml_surface, delete_resqml_objects
