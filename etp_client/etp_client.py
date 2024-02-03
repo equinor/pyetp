59f547cf10901dcfb2919694d0a7d8d2df0558c3
andberge <65176066+andberge@users.noreply.github.com>
2024-02-03 12:09:13 +0000

    async testing framwork

--- etp_client/etp_client.py
+++ etp_client/etp_client.py
@@ -234,7 +234,7 @@ async def upload_resqml_surface(
         )
 
     # Return the uri's of the three uploaded objects.
-    return rddms_urls
+    return tuple(rddms_urls)
 
 
 async def download_array(
