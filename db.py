ec68d5791cb34106df09d2ed92b215c1932d9594
andberge <65176066+andberge@users.noreply.github.com>
2024-02-06 15:06:06 +0000

    fakeredis as depdendency injected

--- db.py
+++ db.py
@@ -4,5 +4,6 @@ import redis.asyncio as redis
 
 pool = redis.ConnectionPool.from_url(environ.get('REDISHOST', 'redis://localhost:6379'))
 
+
 def get_cache():
     return redis.Redis.from_pool(pool)
