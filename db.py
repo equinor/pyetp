8b6dcbc3a2decf04c6d5ff01c70b194816d85d96
andberge <65176066+andberge@users.noreply.github.com>
2024-02-29 10:47:02 +0100

    more leanient redis config pickup (#168)

--- db.py
+++ db.py
@@ -3,6 +3,9 @@ import redis.asyncio as redis
 
 from map_api.config import SETTINGS
 
+if SETTINGS.redis_password is not None:
+    SETTINGS.redis_dns.password = SETTINGS.redis_password
+
 pool = redis.ConnectionPool.from_url(SETTINGS.redis_dns)
 
 
