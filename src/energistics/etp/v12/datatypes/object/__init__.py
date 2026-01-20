9a4c4e9e9818d8bdcb087985392c92a1e2781429
Øyvind Sigmundson Schøyen <8521134+Schoyen@users.noreply.github.com>
2026-01-20 21:25:37 +0100

    Start re-importing etp-messages and objects

---
+++ src/energistics/etp/v12/datatypes/object/__init__.py
@@ -0,0 +1,20 @@
+from etptypes.energistics.etp.v12.datatypes.object.context_info import ContextInfo
+from etptypes.energistics.etp.v12.datatypes.object.context_scope_kind import (
+    ContextScopeKind,
+)
+from etptypes.energistics.etp.v12.datatypes.object.data_object import DataObject
+from etptypes.energistics.etp.v12.datatypes.object.dataspace import Dataspace
+from etptypes.energistics.etp.v12.datatypes.object.relationship_kind import (
+    RelationshipKind,
+)
+from etptypes.energistics.etp.v12.datatypes.object.resource import Resource
+
+__all__ = [
+    "DataObject",
+    "Resource",
+    "Dataspace",
+    "ContextInfo",
+    "ContextScopeKind",
+    "RelationshipKind",
+    "Resource",
+]
