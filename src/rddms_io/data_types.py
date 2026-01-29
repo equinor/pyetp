0f0df255d4d2571ec9fd6283102d03badd30c614
Øyvind Sigmundson Schøyen <8521134+Schoyen@users.noreply.github.com>
2026-01-29 20:38:42 +0100

    Remove unused imports

--- src/rddms_io/data_types.py
+++ src/rddms_io/data_types.py
@@ -1,9 +1,6 @@
-import datetime
 import typing
-from dataclasses import dataclass
 
-from energistics.etp.v12.datatypes import DataValue
-from energistics.etp.v12.datatypes.object import Dataspace, Edge, Resource
+from energistics.etp.v12.datatypes.object import Edge, Resource
 
 
 class LinkedObjects(typing.NamedTuple):
